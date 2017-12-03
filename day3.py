cb85f51921f6f2dd49698342bee51f860df1d163
Hale Steven <sthale@lantm.intra.local>
2017-12-03 22:48:32 +0100

    better turn

--- day3.py
+++ day3.py
@@ -23,18 +23,12 @@ def val(next):
 def perform(previous, current) :
 	delta_x = current[0] - previous[0]
 	delta_y = current[1] - previous[1]
-	
-	if delta_x != 0:
-		if (current[0], current[1] + delta_x) not in visited:
-			next =  (current[0], current[1] + delta_x)
-		else:
-			next =  (current[0] + delta_x, current[1])
 
-	if delta_y != 0:
-		if (current[0]  - delta_y, current[1]) not in visited:
-			next =  (current[0] - delta_y, current[1] )
-		else:
-			next =  (current[0], current[1] + delta_y)
+	pointToLeft = tuple(map(sum, zip(current, (-delta_y, delta_x))))
+	if pointToLeft not in visited:
+		next = pointToLeft
+	else:
+		next = tuple(map(sum, zip(current, (delta_x, delta_y))))
 
 	visited[next] = val(next)
 	return (current, next, visited[next])
