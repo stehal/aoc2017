1035516238e2c5eb99cb8096e966ff295f92c9c8
Hale Steven <sthale@lantm.intra.local>
2017-12-03 08:16:45 +0100

    part 2

---
+++ day3.py
@@ -0,0 +1,60 @@
+
+visited = {(0,0):1, (1,0):1}
+ 
+previous = (0,0)
+current = (1, 0)
+
+def adjacent(p):
+	adj = set()
+	adj.add((p[0], p[1] + 1))
+	adj.add((p[0] + 1, p[1]))
+	adj.add((p[0] + 1, p[1] + 1))
+	adj.add((p[0] + 1, p[1] - 1))
+	adj.add((p[0] - 1 , p[1] + 1))
+	adj.add((p[0] - 1, p[1] - 1))
+	adj.add((p[0], p[1] - 1))
+	adj.add((p[0] - 1, p[1]))
+	return adj
+
+def val(next):
+	adj = adjacent(next)
+	r = 0
+	for  p in adj:
+		if p in visited:
+			r  += visited[p]
+	return r
+	
+def perform(previous, current) :
+	delta_x = current[0] - previous[0]
+	delta_y = current[1] - previous[1]
+	if delta_x != 0:
+		if (current[0], current[1] + delta_x) not in visited:
+			next =  (current[0], current[1] + delta_x)
+		else:
+			next =  (current[0] + delta_x, current[1])
+
+	if delta_y != 0:
+		if (current[0]  - delta_y, current[1]) not in visited:
+			next =  (current[0] - delta_y, current[1] )
+		else:
+			next =  (current[0], current[1] + delta_y)
+
+	
+	value = val(next)
+	visited[next] = value
+	previous = current
+	current = next
+	return (previous, current, value)
+
+value = 1
+while value < 265149:
+	t = perform(previous, current)
+	previous = t[0]
+	current = t[1]
+	value = t[2]
+
+		
+print(visited[current])
+
+
+
